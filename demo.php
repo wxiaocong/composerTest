4fcd7be09796a2e646a5f92756e2f3774a71045b
441549088 <441549088@qq.com>
2019-11-19 11:30:53 +0800

    base

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wong
+ * Date: 2019/11/19
+ * Time: 11:24
+ */
+require_once './vendor/autoload.php';
+use Abstarfish\Starfish\Tools;
+Tools::Hello();
---
+++ src/Abstarfish/Starfish/Tools.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wong
+ * Date: 2019/11/19
+ * Time: 11:02
+ */
+
+namespace Abstarfish\Starfish;
+
+Class Tools{
+    public function __construct()
+    {
+    }
+
+    public static function Hello(){
+        echo 'Hello world';
+    }
+}
